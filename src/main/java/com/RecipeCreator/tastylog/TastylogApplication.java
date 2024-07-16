345f53c059b2ab3add3b1386265a1c32a212164c
higromit <navema1234@gmail.com>
2024-07-16 16:07:48 +0900

    init

---
+++ src/main/java/com/RecipeCreator/tastylog/TastylogApplication.java
@@ -0,0 +1,13 @@
+package com.RecipeCreator.tastylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastylogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastylogApplication.class, args);
+	}
+
+}
